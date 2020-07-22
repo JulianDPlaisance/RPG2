20906b04141d0ba6340a7b61fd0af8d06202c099
Julian Dane Plaisance <jplais8@lsu.edu>
2020-07-22 12:43:33 -0500

    Initial commit

---
+++ Source/RPG2/RPG2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RPG2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG2_API ARPG2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
